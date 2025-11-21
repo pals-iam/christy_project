00caad0ebc495479d331567dea300a35826d534b
Denis <palsdenisworking@yahoo.com>
2025-11-21 19:40:39 +0000

    other_retailers_link_added; text-content_changes

--- website/context_processors.py
+++ website/context_processors.py
@@ -2,6 +2,6 @@ def global_variables(request):
     return {
         'site_name': 'Christiana Catherine',
         'domain': 'christianacatherine.co.uk',
-        'amazon_link': 'PAIN-PURPOSE-Transforming-Trauma-Triumph/dp/B0FYZCF7F4',
+        'other_retailers_link': 'https://christianacatherine.gumroad.com/l/pain2purpose?_gl=1*1r86vub*_ga*MTI5NTM0Njk4My4xNzYzMjI4Mjk0*_ga_6LJN6D94N6*czE3NjM2NTc0NjUkbzMkZzAkdDE3NjM2NTc0NjUkajYwJGwwJGgw',
         'ASIN': 'B0FYZCF7F4',
     }
